bfe22f4ca805cbb059c2dc10ff0329ac93026b61
Carl Avidano <cavidano@gmail.com>
2022-09-18 20:07:40 -0400

    Update AccordionItem.js

--- src/natura11y/modules/accordion/AccordionItem.js
+++ src/natura11y/modules/accordion/AccordionItem.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 
+import { v4 as uuidv4 } from 'uuid';
+
+
 const AccordionItem = ( props ) => {
 
     const {
@@ -7,7 +10,7 @@ const AccordionItem = ( props ) => {
         children = (<p>Accordion Content</p>),
         activeAccordion = null,
         handleClick = handleClick,
-        id
+        id = uuidv4()
     } = props;
 
     useEffect(() => {
