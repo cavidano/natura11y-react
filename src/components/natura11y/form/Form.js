ccfc32d37a537c0dcadd841fc9fe01d9a6d65475
Carl Avidano <cavidano@gmail.com>
2022-10-16 06:21:38 -0400

    conditional label

--- src/components/natura11y/form/Form.js
+++ src/components/natura11y/form/Form.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import RequiredIndicator from './RequiredIndicator';
+import FormEntry from './FormEntry';
 
 const Form = () => {
   return (
     <form>
         <RequiredIndicator />
+        <FormEntry />
+        <FormEntry />
     </form>
   );
 }
--- src/components/natura11y/form/FormEntry.js
+++ src/components/natura11y/form/FormEntry.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
-const FormEntry = ({ children }) => {
+import classNames from 'classnames';
+
+const FormEntry = ( props ) => {
+
+	const {
+
+		labelText = 'Label',
+		labelVisible = true,
+		helpText = 'Include first and last name'
+	
+	} = props;
+
+	const screenReaderOnly = classNames(
+		{ 
+			'screen-reader-only' : !labelVisible
+		}
+	);
+
 	return (
 		<div className='form-entry'>
-			{children}
+
+			<label class='form-entry__field'>
+				<span class={`form-entry__field__label ${screenReaderOnly}`}>
+				{labelText}
+				</span>
+				<span class='form-entry__field__input'>
+					<input
+						type='text'
+						name='textInputExample'
+						id='first-and-last-name'
+						aria-describedby='help-first-and-last-name'
+					/>
+				</span>
+			</label>
+
+			{helpText && (
+				<small class='form-entry__help' id='help-first-and-last-name'>
+					{helpText}
+				</small>
+			)}
+
 		</div>
 	);
 };
